2dfbea7da9440f9e267766cd8a83ec59a64b9ba6
Glenn Sparkes <sparkesg1@nku.edu>
2014-02-14 21:01:43 +0000

    add attendance view link

--- db/schema.rb
+++ db/schema.rb
@@ -15,11 +15,10 @@ ActiveRecord::Schema.define(version: 20140214084314) do
 
   create_table "attendances", force: true do |t|
     t.date     "attended_on"
-    t.datetime "now"
     t.integer  "seat"
+    t.integer  "student_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "student_id"
   end
 
   create_table "current_users", force: true do |t|
