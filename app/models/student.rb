4f0bedc68dbf9c722625cf827c2ee507a42e54b1
Glenn Sparkes <sparkesg1@nku.edu>
2014-02-21 13:20:22 +0000

    add table

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,11 @@ class Student < ActiveRecord::Base
   has_secure_password
   validates_presence_of :password, :on => :create
   has_many :attendances
+  def self.in_seat(seat, date)
+    Student.joins(:attendances).where(attendances: {seat: seat, attended_on: date})
+  end
+
+  def self.absent(date)
+    Student.joins(:attendances).where.not(attendances: {attended_on: date})
+  end
 end
